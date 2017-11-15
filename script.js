43655c1081d00571bf33340cdb644c6e39434952
Denis Karakchiev <uokersam@gmail.com>
2017-11-15 21:45:53 +0300

    commit

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
-let el = document.querySelector('.experiment-element');
+'use strict';
 
-if (el.matches === undefined) {
-  console.log('need polyfill');
-  Object.defineProperty(Element.prototype, 'matches', {
-    value: Element.prototype.matchesSelector || Element.prototype.webkitMatchesSelector,
-  });
-} else {
-  console.log('doesn\'t need a polyfill');
+document.documentElement.children[1].children[0].children[0].textContent = 'this element gets by \'document.documentElement.children[1].children[0].children[0]\'';
+
+if (document.querySelectorAll('.query-test-elem')[1].hasChildNodes()) {
+  document.querySelectorAll(
+      '.query-test-elem')[1].innerHTML = 'first of \'.query-test-elem\' has child nodes';
 }
 
-if (el.matches('.experiment-element')) {
-  console.log('matches');
-} else {
-  console.log('not matches');
+// Напишите код, который выделит все ячейки в таблице по диагонали.
+let rows = document.querySelectorAll('table tr');
+
+rows.forEach(function(row, i) {
+  paintTd(row.children[i]);
+});
+
+function paintTd(td) {
+  td.style.cssText = 'background-color: red; color: white;';
 }
+
+// polifyll for el.closest();
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(el) {
+    let target = this;
+
+    while (target) {
+      if (target.matches(el)) return target;
+      else target = target.parentElement();
+    }
+  };
+}
+
+// let widget = document.querySelector('#widget');
+
+// let widgetName = widget.getAttribute('data-widget-name');
+
+let links = document.querySelectorAll('.external-links-list a');
+
+links.forEach(function(el) {
+  let href = el.getAttribute('href');
+
+  if (href.indexOf('://') === -1) return false;
+  else if (href.indexOf('internal.com') !== -1) return false;
+
+  el.classList.add('external');
+});
+
+// Напишите функцию removeChildren, которая удаляет всех потомков элемента.
+function removeChildren(elem) {
+  while (elem.children[0]) {
+    elem.removeChild(elem.children[0]);
+  }
+}
+
+document.querySelector('.remove-children-test__btn').addEventListener('click', function() {
+  removeChildren(document.querySelector('.remove-children-test')); // очищает список
+});
