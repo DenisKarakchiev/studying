f6aac9ca00d806284593025d0962cdcce17accee
Denis Karakchiev <uokersam@gmail.com>
2017-12-07 19:41:33 +0300

    learning basics of node.js

--- frontend/script.js
+++ frontend/script.js
@@ -3,21 +3,32 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
   let sectors = document.querySelectorAll('.eizens__block');
 
+  let inputs = document.querySelectorAll('.eizens__block-input');
   sectors.forEach(function(el) {
 
     el.addEventListener('click', function() {
-      let input = prompt('Enter the task');
-      let inputEl = document.createElement('div');
-      inputEl.classList.add('task-el');
-
-      inputEl.textContent = input;
-
-      this.appendChild(inputEl);
+      this.querySelector('.eizens__block-input').focus();
     });
-
   });
 
+  inputs.forEach(function(el) {
+    el.addEventListener('keypress', function(event) {
+      if (event.key === 'Enter') {
+        let value   = this.value;
+        let inputEl = document.createElement('div');
+        inputEl.classList.add('task-el');
+        inputEl.textContent = value;
+        this.parentNode.appendChild(inputEl);
+        this.value = '';
+      }
+    });
+  });
 
+  let menu    = document.querySelector('.menu');
+  let menuBtn = document.querySelector('.menu__button');
 
+  menuBtn.addEventListener('click', function() {
+    menu.classList.toggle('menu_visible');
+  });
 }
 
